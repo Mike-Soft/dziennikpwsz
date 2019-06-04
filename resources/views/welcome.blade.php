4f577f157659305ab9dfa894db042fe58fd111d8
Mike-Soft <Mike-Soft@users.noreply.github.com>
2019-06-05 01:32:26 +0200

    nowy komitek

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel testowy
+                    Laravel testowy test
                 </div>
 
                 <div class="links">
