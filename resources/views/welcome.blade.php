dd82012f310a6539ae89bfd79fee2feea409d96d
Mike-Soft <Mike-Soft@users.noreply.github.com>
2019-06-05 01:23:34 +0200

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel testowy
                 </div>
 
                 <div class="links">
