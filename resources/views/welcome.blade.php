4df9d0249c8314d622594dcee6f89e389c4fc361
Mike-Soft <49808920+Mike-Soft@users.noreply.github.com>
2019-06-09 00:34:56 +0200

    Update welcome.blade.php
    n

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel test gita
+                    Laravel test gitara
                 </div>
 
                 <div class="links">
