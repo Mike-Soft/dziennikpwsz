7c1c6b778608e83eb2d0b1156acb9d9384e78461
Mike-Soft <Mike-Soft@users.noreply.github.com>
2019-06-13 11:41:42 +0200

    nowy komitek

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -14,8 +14,8 @@ class SubjectController extends Controller
     {
         $users = DB::table('users')->orderBy('surname')->where('class', $class)->get();
         $sub = DB::table('subjects')->where('name', $subject)->first();
-        $subs = DB::table('subjects')->where('name', $subject)->first();
-        $marks = DB::table('subject_user')->where('subject_id', $subs->id)->get();
+        $subs = DB::table('subjects')->where('name', $subject)->first('id');
+        $marks = DB::table('subject_user')->where('subject_id', $subs)->get();
 //        $allusers = DB::table('users')->orderBy('surname')->get();
 
         return view('teacher.clas')->with ('users', $users)->with('sub', $sub)->with('marks', $marks)->with('class', $class);
