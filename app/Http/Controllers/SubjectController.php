517b3847ab86b1969bdce8c846ef805adc39a4b7
Mike-Soft <Mike-Soft@users.noreply.github.com>
2019-06-14 04:34:46 +0200

    nowy komitek

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use App\Clas;
 use App\Subject;
+use App\SubjectUser;
 
 class SubjectController extends Controller
 {
@@ -14,10 +15,10 @@ class SubjectController extends Controller
     {
         $users = DB::table('users')->orderBy('surname')->where('class', $class)->get();
         $sub = DB::table('subjects')->where('name', $subject)->first();
-        $subs = DB::table('subjects')->where('name', $subject)->first('id');
-        $marks = DB::table('subject_user')->where('subject_id', $subs);
+        $subs = DB::table('subjects')->where('name', $subject)->first();
+        $marks = DB::table('subject_user')->where('subject_id', $sub->id)->get();
 //        $allusers = DB::table('users')->orderBy('surname')->get();
 
-        return view('teacher.clas')->with ('users', $users)->with('sub', $sub)->with('marks', $marks)->with('class', $class)->with('subid', $subs);
+        return view('teacher.clas')->with ('users', $users)->with('sub', $sub)->with('marks', $marks)->with('class', $class)->with('subid', $sub->id);
     }
 }
